8bf50e7be266ae26ca6557d69ddb05adf5f345b1
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-10-26 03:53:19 +0300

    27. Remove Element

---
+++ Array & String/27. Remove Element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+var removeElement = function(nums, val) {
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i <= j) {
+        if (nums[i] === val) {
+            nums[i] = nums[j];
+            j--;
+        } else {
+            i++;
+        }
+    }
+    
+    return i;
+};
